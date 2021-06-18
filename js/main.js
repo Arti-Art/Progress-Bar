12b45d9e2d111d4f7d9b108718e2a32f1cbb4c5f
Arti <art.kouznetsov@gmail.com>
2021-06-18 13:05:29 +0200

    Testing media queries

--- js/main.js
+++ js/main.js
@@ -1,63 +1,68 @@
-let toc = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.table-of-contents',
-    start: 'top 10%', // start when center of our trigger element reaches %fromTop of viewport
-    // markers: true,
-    endTrigger: '.grid-right',
-    end: "bottom 50%",
-    pin: true
+ScrollTrigger.matchMedia({
+  "(min-width: 800px)": function () {
+    
+    let toc = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.table-of-contents',
+        start: 'top 10%', // start when center of our trigger element reaches %fromTop of viewport
+        // markers: true,
+        endTrigger: '.grid-right',
+        end: "bottom 50%",
+        pin: true
+      }
+    });
+    
+    let progressA = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.section-a',
+        start: 'top 50%',
+        markers: true,
+        endTrigger: '.section-a',
+        end: "bottom 30%",
+        toggleClass: "toc-selected",
+        scrub: 0.5
+      }
+    });
+    progressA.from('.progress-a', { x: -200 });
+    
+    let progressB = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.section-b',
+        start: 'top 50%',
+        markers: true,
+        endTrigger: '.section-b',
+        end: "bottom 30%",
+        scrub: 0.5
+      }
+    });
+    
+    progressB.from('.progress-b', { x: -320 });
+    
+    let progressC = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.section-c',
+        start: 'top 50%',
+        markers: true,
+        endTrigger: '.section-c',
+        end: "bottom 30%",
+        scrub: 0.5
+      }
+    });
+    
+    progressC.from('.progress-c', { x: -320 });
+    
+    let progressD = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.section-d',
+        start: 'top 50%',
+        markers: true,
+        endTrigger: '.section-d',
+        end: "bottom 30%",
+        scrub: 0.5
+      }
+    });
+    
+    progressD.from('.progress-d', { x: -320 });
+
   }
-});
-
-let progressA = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.section-a',
-    start: 'top 50%',
-    // markers: true,
-    endTrigger: '.section-a',
-    end: "bottom 30%",
-    // toggleClass: "toc-selected", // put on the right selector if needed
-    scrub: 0.5
-  }
-});
-
-progressA.from('.progress-a', { x: -320 });
-
-let progressB = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.section-b',
-    start: 'top 50%',
-    // markers: true,
-    endTrigger: '.section-b',
-    end: "bottom 30%",
-    scrub: 0.5
-  }
-});
-
-progressB.from('.progress-b', { x: -320 });
-
-let progressC = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.section-c',
-    start: 'top 50%',
-    // markers: true,
-    endTrigger: '.section-c',
-    end: "bottom 30%",
-    scrub: 0.5
-  }
-});
-
-progressC.from('.progress-c', { x: -320 });
-
-let progressD = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.section-d',
-    start: 'top 50%',
-    // markers: true,
-    endTrigger: '.section-d',
-    end: "bottom 30%",
-    scrub: 0.5
-  }
-});
-
-progressD.from('.progress-d', { x: -320 });
+})
